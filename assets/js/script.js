449057cd5cb2fbaf0aaf4f4efe5358821d7ac1e8
11florin <albuflorin929@yahoo.co.uk>
2026-03-23 14:00:37 +0000

    Display addition to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame('addition');
 });
 
 
@@ -21,10 +23,17 @@ document.addEventListener('DOMContentLoaded', function() {
  * The main game 'loop', called when the script is first loaded 
  * and after the user's answers hae been processed
  */
-function runGame() {
+function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1  = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer() {
 
@@ -38,8 +47,10 @@ function incrementScore() {
 function incrementWrongAnswer(){
 
 }
-function displayAditionQuestion() {
-
+function displayAditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 function displaySubtractQuestion() {
 
