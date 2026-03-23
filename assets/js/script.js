96d0325a19dfcd97df2d677516a8a31977cc4216
11florin <albuflorin929@yahoo.co.uk>
2026-03-23 13:31:58 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore() {
+    
+}
+function incrementWrongAnswer(){
+
+}
+function displayAditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplayQuestion() {
+
+}
